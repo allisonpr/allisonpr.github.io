8abea1f38d81a565afb4ac9109dea1988a3f94b3
allisonpr <0618paredesallison@gmail.com>
2024-07-16 16:43:04 -0400

    Update basicMap.js

--- basicMap.js
+++ basicMap.js
@@ -21,7 +21,7 @@ L.marker([35.2176665,-80.831473], {
 <a href="https://www.cpcc.edu"> Click Here to Visit CPCC'S Website</a><br>
 <iframe width="280" height="160" src="https://www.youtube.com/embed/WEz48U2XJ0I?si=WSE7CYfLduPMRMDm" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 <hr>
-<p>[URL:https://youtu.be/WEz48U2XJ0I?si=HK88x1ct3ETFwx7X]
+<p>[URL:https://youtu.be/WEz48U2XJ0I?si=HK88x1ct3ETFwx7X]</p>
 </center>
 `)
   .addTo(map);
